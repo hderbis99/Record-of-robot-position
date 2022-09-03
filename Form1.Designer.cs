6a6b0b24dd9f3785b29ddeb7cbe24732c3e41fac
Hubert <106434647+hderbis99@users.noreply.github.com>
2022-09-03 18:59:10 +0200

    Program to record the position the robot is in

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿
+namespace Program_robot
+{
+    partial class Robot1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.Main = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Configuration = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Information = new System.Windows.Forms.TabPage();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TabControl.SuspendLayout();
+            this.Main.SuspendLayout();
+            this.Configuration.SuspendLayout();
+            this.Information.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.Main);
+            this.TabControl.Controls.Add(this.Configuration);
+            this.TabControl.Controls.Add(this.Information);
+            this.TabControl.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TabControl.Location = new System.Drawing.Point(12, 12);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(776, 426);
+            this.TabControl.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.Main.Controls.Add(this.textBox4);
+            this.Main.Controls.Add(this.textBox3);
+            this.Main.Controls.Add(this.textBox2);
+            this.Main.Controls.Add(this.textBox1);
+            this.Main.Controls.Add(this.label5);
+            this.Main.Controls.Add(this.label4);
+            this.Main.Controls.Add(this.label3);
+            this.Main.Controls.Add(this.label2);
+            this.Main.Controls.Add(this.label1);
+            this.Main.Controls.Add(this.button5);
+            this.Main.Location = new System.Drawing.Point(4, 23);
+            this.Main.Name = "Main";
+            this.Main.Padding = new System.Windows.Forms.Padding(3);
+            this.Main.Size = new System.Drawing.Size(768, 399);
+            this.Main.TabIndex = 0;
+            this.Main.Text = "Main";
+            this.Main.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(646, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(76, 32);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(513, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(76, 32);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(170, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(76, 32);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(37, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(76, 32);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(307, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 27);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Save position";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(663, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 27);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "S4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(530, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 27);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "S3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 27);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "S2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "S1";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(294, 220);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 44);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Click";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Configuration
+            // 
+            this.Configuration.Controls.Add(this.label8);
+            this.Configuration.Controls.Add(this.button9);
+            this.Configuration.Controls.Add(this.button8);
+            this.Configuration.Controls.Add(this.label7);
+            this.Configuration.Controls.Add(this.textBox5);
+            this.Configuration.Location = new System.Drawing.Point(4, 23);
+            this.Configuration.Name = "Configuration";
+            this.Configuration.Padding = new System.Windows.Forms.Padding(3);
+            this.Configuration.Size = new System.Drawing.Size(768, 399);
+            this.Configuration.TabIndex = 1;
+            this.Configuration.Text = "Configuration";
+            this.Configuration.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(302, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 27);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Serial port:";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(362, 189);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(121, 75);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "Standby";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Standby_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Green;
+            this.button8.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(236, 189);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(120, 75);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Operate";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Operate_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(231, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 26);
+            this.label7.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(307, 131);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 34);
+            this.textBox5.TabIndex = 1;
+            // 
+            // Information
+            // 
+            this.Information.Controls.Add(this.label11);
+            this.Information.Controls.Add(this.label10);
+            this.Information.Controls.Add(this.button7);
+            this.Information.Location = new System.Drawing.Point(4, 23);
+            this.Information.Name = "Information";
+            this.Information.Padding = new System.Windows.Forms.Padding(3);
+            this.Information.Size = new System.Drawing.Size(768, 399);
+            this.Information.TabIndex = 2;
+            this.Information.Text = "Information";
+            this.Information.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(289, 258);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(190, 26);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "about this program";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(169, 232);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(417, 26);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Click here when you want more information";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(269, 117);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(210, 112);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Click";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.aboutAuthor);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Robot1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TabControl);
+            this.Name = "Robot1";
+            this.Text = "Planar robot";
+            this.TabControl.ResumeLayout(false);
+            this.Main.ResumeLayout(false);
+            this.Main.PerformLayout();
+            this.Configuration.ResumeLayout(false);
+            this.Configuration.PerformLayout();
+            this.Information.ResumeLayout(false);
+            this.Information.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage Main;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TabPage Configuration;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TabPage Information;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program_robot
+{
+    public partial class Robot1 : Form
+    {
+        private SerialPort _sp = new SerialPort();
+
+        private string _portCom;
+        private string _servoCords;
+
+        private const string PATH_TO_SAVE = @"..\..\\Data\\";
+        private const string SEPARATOR = ",";
+
+        public Robot1()
+        {
+            InitializeComponent();
+        }
+
+        private void Operate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                _portCom = textBox5.Text;
+                if(_portCom.Equals("COM4"))
+                {
+                    _sp.PortName = _portCom;
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect COM");
+                    return;
+                }
+                //_sp = new SerialPort();
+                _sp.BaudRate = 9600;//!!
+                _sp.Open();
+                timer1.Enabled = true;
+                MessageBox.Show("You open the port");
+
+                _servoCords = _sp.ReadLine();
+                _servoCords = _servoCords.TrimStart(':');
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Standby_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_sp != null)
+                {
+                    _sp.Close();
+                    timer1.Enabled = false;
+                    MessageBox.Show("You close the port");
+                }
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var d = _sp.ReadLine();
+            char[] MyChar = { ':', 'o', 'n', 'e', 't', 'w', 'h', 'r', 'f', 'u' };
+
+            if (d.StartsWith("one:"))
+            {
+                textBox1.Text = d.TrimStart(MyChar);
+            }
+
+            if (d.StartsWith("two:"))
+            {
+                textBox2.Text = d.TrimStart(MyChar);
+            }
+
+            if (d.StartsWith("three:"))
+            {
+                textBox3.Text = d.TrimStart(MyChar);
+            }
+
+            if (d.StartsWith("four:"))
+            {
+                textBox4.Text = d.TrimStart(MyChar);
+            }
+
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (IsAnyServoEmpty())
+            {
+                MessageBox.Show("No data to record");
+                return;
+            }
+
+            Directory.CreateDirectory(PATH_TO_SAVE);
+
+            
+            var text = new StreamWriter(PATH_TO_SAVE + DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss" + ".CSV"));
+
+            using(text)
+            {
+                var strBuilder = new StringBuilder();
+
+                strBuilder.Append("Servo1,Servo2,Servo3,Servo4");
+                strBuilder.Append("\r");
+                strBuilder.Append(RemoveNewLine(textBox1.Text)).Append(SEPARATOR);
+                strBuilder.Append(RemoveNewLine(textBox2.Text)).Append(SEPARATOR);
+                strBuilder.Append(RemoveNewLine(textBox3.Text)).Append(SEPARATOR);
+                strBuilder.Append(RemoveNewLine(textBox4.Text));
+
+                var file = strBuilder.ToString();
+
+                text.Write(file);
+
+                text.Close();
+            }
+
+            MessageBox.Show("SAVED");
+        }
+
+        private static string RemoveNewLine(string str) => str.Replace("\r", "");
+        private  bool IsAnyServoEmpty()
+        {
+            var servoes = new List<string> { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text };
+
+            if(servoes.Any(x => x.Equals(string.Empty)))  //sprawdza po kolei kazda wartosc z tablicy czy jest rowna string.empty
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void aboutAuthor(object sender, EventArgs e)
+        {
+            string message = "The author of this program is student Hubert Derbis.\nProject was created in cooperation with\nUniversity of Zielona Gora.";
+            string title = "Information about author";
+            MessageBox.Show(message, title);
+        }
+    }
+}
+
+
